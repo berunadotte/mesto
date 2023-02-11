f340323601c08f25239a20bce4886fdd17bc509b
DoriDori <berunadotte@yandex.ru>
2023-02-11 07:06:04 +0300

    feat: hover effectr

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+let addButton = document.querySelector('.profile__edit-button')
+let likeButton = document.querySelectorAll('.card__like-button')
+
+likeButton.addEventListener('click', popupEditProfile)
+
+function popupEditProfile() {
+  console.log('piu piu');
+}

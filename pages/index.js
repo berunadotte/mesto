b7b24720544cf894b711ede29575df40a8b2497b
DoriDori <berunadotte@yandex.ru>
2023-02-13 01:06:16 +0300

    feat: desktop and mobile popup html layout

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 let addButton = document.querySelector('.profile__edit-button')
-let likeButton = document.querySelectorAll('.card__like-button')
+let likeButton = document.querySelector('.card__like-button')
 
-likeButton.addEventListener('click', popupEditProfile)
 
 function popupEditProfile() {
   console.log('piu piu');
 }
+
+likeButton.addEventListener('click', popupEditProfile)

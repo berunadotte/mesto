4745f5e4d564c51de281da73d04f6bc1c9ea5850
DoriDori <berunadotte@yandex.ru>
2023-02-24 01:25:03 +0300

    feat: add card template

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,30 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg',
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg',
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg',
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg',
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg',
+  },
+]
+
 let profileEditButton = document.querySelector('.profile__edit-button')
 let closeEditButton = document.querySelector('.popup__close-button')
 let popupWindow = document.querySelector('.popup')
@@ -6,6 +33,24 @@ let nameInput = document.querySelector('.popup__input_name_value')
 let jobInput = document.querySelector('.popup__input_job_value')
 let profileName = document.querySelector('.profile__title')
 let profileJob = document.querySelector('.profile__subtitle')
+let cardsList = document.querySelector('.cards__list')
+const cardImage = document.querySelector('.card__image')
+const cardLabel = document.querySelector('.card__label')
+
+
+
+ initialCards.forEach( (arrCardsElement) => {
+ 
+//   cardImage.src = arrCardsElement.link
+//   cardImage.alt = arrCardsElement.name
+
+//   card.append(cardImage)
+
+
+//   cardsList.append(card)
+ 
+})
+// console.log(cardsList)
 
 function openEditProfile() {
   // открытие попапа по нажатию на кнопку редактирования
@@ -36,3 +81,5 @@ function handleFormSubmit(evt) {
 profileEditButton.addEventListener('click', openEditProfile)
 closeEditButton.addEventListener('click', closeEditProfile)
 formElement.addEventListener('submit', handleFormSubmit)
+
+

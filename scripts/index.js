095d641650f257b4ce05c7e58e977a00c94f7d21
berunadotte <berunadotte@gmail.com>
2023-02-27 16:58:14 +0300

    fix: change popup class name

--- scripts/index.js
+++ scripts/index.js
@@ -26,20 +26,22 @@ const initialCards = [
 ]
 
 let profileEditButton = document.querySelector('.profile__edit-button')
-let closeEditButton = document.querySelector('.popup__close-button')
-let popupWindow = document.querySelector('.popup')
-let formElement = document.querySelector('.popup__form')
-let nameInput = document.querySelector('.popup__input_name_value')
-let jobInput = document.querySelector('.popup__input_job_value')
+let closeEditButton = document.querySelector('.popup-edit-profile__close-button')
+let popupWindow = document.querySelector('.popup-edit-profile')
+let formElement = document.querySelector('.popup-edit-profile__form')
+let nameInput = document.querySelector('.popup-edit-profile__input_name_value')
+let jobInput = document.querySelector('.popup-edit-profile__input_job_value')
 let profileName = document.querySelector('.profile__title')
 let profileJob = document.querySelector('.profile__subtitle')
 
 let popupAdding = document.querySelector('.popup-new-card')
-
+let addCardButton = document.querySelector('.profile__add-button')
+let cardNameInput = document.querySelector('.popup__input_card-name_value')
+let cardLinkInput = document.querySelector('.popup__input_image-link_value')
+let createCardButton = document.querySelector('.popup__create-button')
 
 
 initialCards.forEach((arrCardsElement) => {
-
   const cardsTemplate = document.querySelector('.cards__template').content
   const cardsList = document.querySelector('.cards__list')
   const cardElement = cardsTemplate.querySelector('.card').cloneNode(true)
@@ -54,16 +56,7 @@ initialCards.forEach((arrCardsElement) => {
 
 function openEditProfile() {
   // открытие попапа по нажатию на кнопку редактирования
-  popupWindow.setAttribute('class', 'popup popup_opened')
-
-  nameInput.value = profileName.textContent
-  jobInput.value = profileJob.textContent
-  // присваивание текстовых значений атрибуту value форм ввода данных пользователем
-}
-
-function addNewCard() {
-  // открытие попапа по нажатию на кнопку редактирования
-  popupAdding.setAttribute('class', 'popup-new-card popup_opened')
+  popupWindow.setAttribute('class', 'popup-edit-profile popup-edit-profile_opened')
 
   nameInput.value = profileName.textContent
   jobInput.value = profileJob.textContent
@@ -72,20 +65,40 @@ function addNewCard() {
 
 function closeEditProfile() {
   // закрытие попапа по нажатию на крестик
-  popupWindow.setAttribute('class', 'popup')
+  popupWindow.setAttribute('class', 'popup-edit-profile')
 }
 
 function handleFormSubmit(evt) {
   evt.preventDefault()
   // перехватывает действие по умолчанию при нажатии кнопки сохранить
-
   profileName.textContent = nameInput.value
   profileJob.textContent = jobInput.value
   // записывает значения введенные в форме пользователем в соответствующие текстовые значения элементов DOM дерева
-
   closeEditProfile()
   // вызов функции закрытия формы
 }
+
+
+
+function addNewCard() {
+  // открытие попапа по нажатию на кнопку редактирования
+  popupAdding.setAttribute('class', 'popup popup-new-card popup_opened')
+
+  nameInput.value = profileName.textContent
+  jobInput.value = profileJob.textContent
+  // присваивание текстовых значений атрибуту value форм ввода данных пользователем
+}
+
+function handleAddCard(evt) {
+  evt.preventDefault()
+
+  initialCards(initialCards.length) = {name: cardNameInput, link: cardLinkInput}
+
+}
+
+
+
+
 
 profileEditButton.addEventListener('click', openEditProfile)
 closeEditButton.addEventListener('click', closeEditProfile)

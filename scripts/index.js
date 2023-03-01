d21f1d35f8128fddc9af293b90f1a55e4e8e4a7b
berunadotte <berunadotte@gmail.com>
2023-03-01 17:01:37 +0300

    feat: not working like buttons

--- scripts/index.js
+++ scripts/index.js
@@ -36,27 +36,23 @@ let profileJob = document.querySelector('.profile__subtitle')
 
 let addCardButton = document.querySelector('.profile__add-button')
 let closeAddingPopupButton = document.querySelector('.popup-new-card__close-button')
-let popupAddingCardWindow = document.querySelector ('.popup-new-card')
+let popupAddingCardWindow = document.querySelector('.popup-new-card')
 let newCardFormElement = document.querySelector('.popup-new-card__form')
-let cardNameInput =document.querySelector('.popup-new-card__input_card-name_value')
+let cardNameInput = document.querySelector('.popup-new-card__input_card-name_value')
 let cardLinkInput = document.querySelector('.popup__input_image-link_value')
 
 const cardsTemplate = document.querySelector('.cards__template').content
 const cardsList = document.querySelector('.cards__list')
 
-
-
 initialCards.forEach((arrCardsElement) => {
   const cardElement = cardsTemplate.querySelector('.card').cloneNode(true)
- 
+
   cardElement.querySelector('.card__image').src = arrCardsElement.link
   cardElement.querySelector('.card__image').alt = arrCardsElement.name
   cardElement.querySelector('.card__label').textContent = arrCardsElement.name
 
   cardsList.append(cardElement)
-
 })
-
 
 function openEditProfile() {
   // открытие попапа по нажатию на кнопку редактирования
@@ -90,16 +86,13 @@ function handleFormSubmit(evt) {
   // вызов функции закрытия формы
 }
 
-
-
 function addNewCard() {
-  // открытие попапа по нажатию на кнопку редактирования
+  // открытие попапа по нажатию на кнопку добавления
   popupAddingCardWindow.setAttribute('class', 'popup-new-card popup-new-card_opened')
-
-  nameInput.value = profileName.textContent
-  jobInput.value = profileJob.textContent
-  // присваивание текстовых значений атрибуту value форм ввода данных пользователем
 }
+
+// let cardLikeButtons = cardsList.querySelectorAll('.card__like-button')
+
 
 function handleAddCard(evt) {
   evt.preventDefault()
@@ -107,19 +100,20 @@ function handleAddCard(evt) {
   initialCards[initialCards.length] = {name: cardNameInput.value, link: cardLinkInput.value}
 
   const newCardElement = cardsTemplate.querySelector('.card').cloneNode(true)
- 
+
   newCardElement.querySelector('.card__image').src = cardLinkInput.value
   newCardElement.querySelector('.card__image').alt = cardNameInput.value
   newCardElement.querySelector('.card__label').textContent = cardNameInput.value
 
   cardsList.prepend(newCardElement)
+  cardLikeButtons.
 
   cardLinkInput.value = ''
   cardNameInput.value = ''
 
   closeAddingCard()
-
 }
+
 
 
 profileEditButton.addEventListener('click', openEditProfile)
@@ -127,5 +121,27 @@ closeEditButton.addEventListener('click', closeEditProfile)
 editProfileFormElement.addEventListener('submit', handleFormSubmit)
 
 closeAddingPopupButton.addEventListener('click', closeAddingCard)
-addCardButton.addEventListener('click', addNewCard )
+addCardButton.addEventListener('click', addNewCard)
 newCardFormElement.addEventListener('submit', handleAddCard)
+
+// cardLikeButtons.forEach((likeButton) => {
+//   let cardLike = likeButton.querySelector('.card__like')
+//   function likeToggle() {
+//     if (cardLike.src.includes('like_button_active')) {
+//      cardLike.src = 'images/like_button.svg'
+//    } else {
+//      cardLike.src = 'images/like_button_active.svg'
+//     }
+//    }
+//   likeButton.addEventListener('click', likeToggle)
+// })
+
+const likeButton = document.querySelector('.card__like-button')
+
+likeButton.addEventListener('click', (evt) => {
+  const eventTarget = evt.target
+  console.log(evt)
+  console.log(eventTarget)
+
+  // eventTarget.querySelector('.card__like').src = 'images/like_button_active.svg'
+})

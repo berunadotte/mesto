6a62bc0ff80ea61b23415136f40aa066bda72093
berunadotte <berunadotte@gmail.com>
2023-03-18 14:36:17 +0300

    feat: access to form input

--- scripts/validate.js
+++ scripts/validate.js
@@ -4,5 +4,12 @@ const popupForms = document.querySelectorAll('.popup__form')
 const popupInputs = document.querySelectorAll('.popup__input')
 
 
+popupInputs.forEach((popupInput) => {
+  popupInput.addEventListener('input', (evt) => {
+    console.log(evt.target.validity.valid)
+  })
+})
+
+
 
 

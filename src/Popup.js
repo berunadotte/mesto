d291bee33ba17eff2fdf5a6c1d9303af3c95982c
admin <admin@DoriDori.local>
2023-05-29 23:38:24 +0300

    feature: make class Popup

--- src/Popup.js
+++ src/Popup.js
@@ -1,30 +1,31 @@
 export default class Popup {
   constructor(popupSelector) {
-    this._popup = document.querySelector(popupSelector)
+    this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
-    this._popup.classList.add('popup_opened')
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
-    this._popup.classList.remove('popup_opened')
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
-  _handleEscClose() {
+  _handleEscClose(evt) {
     if (evt.key === 'Escape') {
-      this.close(document.querySelector('.popup_opened'))
+      this.close();
     }
   }
 
   setEventListeners() {
-    this._popup.addEventListener('mousedown', (evt) => {
-      if (evt.target.classList.contains('popup_opened')) {
-        this.close()
+    this._popup.querySelector('.popup__close-icon').addEventListener('click', () => this.close());
+    this._popup.addEventListener('click', (evt) => {
+      if (evt.target === evt.currentTarget) {
+        this.close();
       }
-      if (evt.target.classList.contains('popup__close-icon')) {
-        this.close()
-      }
-    })
+    });
   }
 }
